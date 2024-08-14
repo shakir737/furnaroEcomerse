49ef3f78c47551f34f118e046d9d269200fd0005
shakir raza <shakirraza737@gmail.com>
2024-08-14 14:39:10 +0300

    changed

--- src/app/api/billing/route.ts
+++ src/app/api/billing/route.ts
@@ -5,7 +5,6 @@ import { BillingInfoInputValidation } from "@/lib/validations";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-
     const inputValidation = BillingInfoInputValidation.safeParse(body);
     
     if (!inputValidation.success) {
