3a46efff04e417222578dc358161f9920682bb0e
shakir raza <shakirraza737@gmail.com>
2024-08-15 16:40:16 +0300

    data base change

--- src/lib/database/connection/mongoose.ts
+++ src/lib/database/connection/mongoose.ts
@@ -1,35 +1,50 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
-const MONGODB_URL = "mongodb+srv://shakirraza737:Anilla621@ecomerse.fxjt0iy.mongodb.net/Application?retryWrites=true&w=majority&appName=Ecomerse";
 
-interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
-}
 
-let cached: MongooseConnection = (global as any).mongoose;
+
+// interface MongooseConnection {
+//   conn: Mongoose | null;
+//   promise: Promise<Mongoose> | null;
+// }
+
+// let cached: MongooseConnection = (global as any).mongoose;
 
 // # Singleton
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
+// if (!cached) {
+//   cached = (global as any).mongoose = {
+//     conn: null,
+//     promise: null,
+//   };
+// }
+let isConnected = false;
 
 export const connectToDatabase = async () => {
-  if (cached.conn) return cached.conn;
+  
+  if (isConnected) {
+    return;
+  }
+  const MONGODB_URL = `mongodb+srv://shakirraza737:Anilla621@ecomerse.fxjt0iy.mongodb.net/Application?retryWrites=true&w=majority&appName=Ecomerse`;
+  try {
+  await mongoose.connect(MONGODB_URL);
+  isConnected = true;
+  console.log("Connected to MongoDB.");
+} catch (error) {
+  console.error("Error connecting to MongoDB:", error);
+  throw error;
+}
+  // if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+  // if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: "furniro",
-      bufferCommands: false,
-    });
+  // cached.promise =
+  //   cached.promise ||
+  //   mongoose.connect(MONGODB_URL, {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   });
 
-  cached.conn = await cached.promise;
+  // cached.conn = await cached.promise;
 
-  return cached.conn;
+  // return cached.conn;
 };
